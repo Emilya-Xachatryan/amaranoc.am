92374569310386b71e2d2db2ca40a3d8831f353d
emilya <emilyaxachatryan.ait@gmail.com>
2025-01-11 23:29:15 +0400

    new name css

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -2,7 +2,8 @@ import React, { useMemo, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import { image1, image2, image3, image4,image5,image6,image7, image8, image9, image10, image11, image12} from '../Class';
 import "../style/Carusel.css"
-import "../style/imageGallery.css"
+import "../style/Gallery.css"
+
 
 const carouselItems = [
   { label: 'Առանձնատներ', icon: 'home.svg', link: '/homes' },
